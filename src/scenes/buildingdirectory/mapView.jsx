e53958802ba1aba9ba0e17c5e67be5228766eb91
Waqas Kureshy <101527504+kgdash116@users.noreply.github.com>
2023-05-01 16:45:22 +0500

    updated mapview
    made addition of adding latitude and longitude, to make addition of building possible. Made changes in the API call to the backend to post longitude and latitude.

--- src/scenes/buildingdirectory/mapView.jsx
+++ src/scenes/buildingdirectory/mapView.jsx
@@ -6,45 +6,32 @@ import {
 } from "@react-google-maps/api";
 import { useState } from "react";
 import "./App.css";
+import { AddBuildingModal } from "./addBuildingModal";
 
-
-
-  
-  const MapView = () => {
-    const { isLoaded } = useLoadScript({
-      googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-    });
-    const [mapRef, setMapRef] = useState();
+const MapView = (props) => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  });
+  const [mapRef, setMapRef] = useState();
   const [isOpen, setIsOpen] = useState(false);
+  const [markers, setMarkers] = useState([]);
+  const [addModalShow, setAddModalShow] = useState(false);
   const [infoWindowData, setInfoWindowData] = useState();
-  const markers = [
-   
-    { address: "Health-Building-Entry1", lat: 37.335776, lng: -121.879080 },
-    { address: "Health-Building-Exit", lat:  37.335652, lng: -121.879235 },
-    { address: "Health-Building-Entry", lat:  37.335853, lng: -121.879241 },
-    { address: "Health-Buildingr-NW-Exit", lat:  37.335529, lng:-121.879037 },
-    { address: "Health-Building-Lobby", lat:  37.335678, lng:-121.879160 },
-    { address: "Art-Building-LeftEntry", lat:  37.336210, lng:-121.880210},
-    { address: "Art-Building-RightEntry", lat:  37.336423, lng:-121.879765},
-    { address: "Art-Building-Exit", lat:  37.335826, lng:-121.880001},
-    { address: "Art-Building-LeftLobby", lat:  37.336005, lng:-121.880173},
-    { address: "Art-Building-LeftLobby", lat:   37.336193, lng:-121.879518},
-    { address: "Music-Building-Entrance", lat:    37.335456, lng:-121.881129},
-    { address: "Concert-Hall-Entrance", lat:     37.335439, lng:-121.880764},
-    { address: "Music-Building-Exit", lat:     37.335860, lng:-121.880984},
-    { address: "Dinning-Commons-Exit", lat:     37.334145, lng:-121.878285},
-    { address: "Dinning-Commons-Entry", lat:      37.333949, lng:-121.8787255},
-    { address: "Engineering-building-Entry", lat:      37.336700, lng:-121.881069},
-    { address: "Engineering-building-exit(Street View)", lat:     37.337165,lng:-121.882475},
-    { address: "Lobby", lat:     37.337361,lng:  -121.881964},
-    
+  const [latitude, setLat] = useState(null);
+  const [longitude, setLng] = useState(null);
 
-  
+  const { buildings } = props;
 
-    
-   
+  let addModalClose = () => setAddModalShow(false);
 
-  ];
+  const onMapClick = (event) => {
+    let lat = event.latLng.lat();
+    let lng = event.latLng.lng();
+    setLat(lat);
+    setLng(lng);
+
+    setAddModalShow(true);
+  };
 
   const onMapLoad = (map) => {
     setMapRef(map);
@@ -67,25 +54,33 @@ import "./App.css";
         <GoogleMap
           mapContainerClassName="map-container"
           onLoad={onMapLoad}
-          onClick={() => setIsOpen(false)}
+          onClick={onMapClick}
+          zoom={20}
+          center={{ lat: 37.33590395941839, lng: -121.87931197105083 }}
         >
-          {markers.map(({ address, lat, lng }, ind) => (
+          <AddBuildingModal
+            lat={latitude}
+            lng={longitude}
+            show={addModalShow}
+            onHide={addModalClose}
+          />
+          {buildings.map(({ building_name, lat, lng }, ind) => (
             <MarkerF
               key={ind}
               position={{ lat, lng }}
-              icon = {{
-                //url: 'https://freesvg.org/img/1499683972.png',
-                url:'https://freesvg.org/img/tv_camera_sign.png',
+              icon={{
+                url: "https://www.svgrepo.com/show/47341/cctv.svg",
+                //"https://freesvg.org/img/tv_camera_sign.png",
                 //url:'https://freesvg.org/img/jcartier-wireless-video-camera.png',
-                scaledSize: new window.google.maps.Size(25, 25)
+                scaledSize: new window.google.maps.Size(25, 25),
               }}
               // {{
               //   url: './Images/camera.png',
               //   scaledSize: new window.google.maps.Size(50, 50),
               // }}
-              
+
               onClick={() => {
-                handleMarkerClick(ind, lat, lng, address);
+                handleMarkerClick(ind, lat, lng, building_name);
               }}
             >
               {isOpen && infoWindowData?.id === ind && (
@@ -94,13 +89,13 @@ import "./App.css";
                     setIsOpen(false);
                   }}
                 >
-                  
-                {<h6 style={{ color: '#6870fa' }}>{infoWindowData.address}</h6> }
-                  
+                  {
+                    <h6 style={{ color: "#6870fa" }}>
+                      {infoWindowData.address}
+                    </h6>
+                  }
                 </InfoWindow>
-
               )}
-              
             </MarkerF>
           ))}
         </GoogleMap>
@@ -108,5 +103,5 @@ import "./App.css";
     </div>
   );
 };
-  
-  export default MapView;
+
+export default MapView;
