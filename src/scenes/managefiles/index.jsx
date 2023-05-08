a1a5ed5d579f3186b90721e44d6bffdbf9dbffdb
Leo Guo <43938469+a819694114@users.noreply.github.com>
2023-05-08 07:21:41 -0700

    play function for files

--- src/scenes/managefiles/index.jsx
+++ src/scenes/managefiles/index.jsx
@@ -1,17 +1,121 @@
 import { Box, Button, Typography} from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
-
+import ReactPlayer from "react-player";
 import Header from "../../components/Header";
 import { useTheme } from "@mui/material";
 import axios from 'axios';
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef} from "react";
+import screenfull from "screenfull";
+
+const format = (seconds) => {
+    if (isNaN(seconds)) {
+      return "00:00";
+    }
+  
+    const date = new Date(seconds * 1000);
+    const hh = date.getUTCHours();
+    const mm = date.getUTCMinutes();
+    const ss = date.getUTCSeconds().toString().padStart(2, "0");
+  
+    if (hh) {
+      return `${hh}:${mm.toString().padStart(2, "0")}:${ss}`;
+    } else {
+      return `${mm}:${ss}`;
+    }
+  };
 
 const ManageFiles = () => {
+  
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   //const axios = require('axios');
+  //Destructure State in other to get the values in it
+  const [playerstate, setPlayerState] = useState({
+    playing: true,
+    muted: true,
+    volume: 0.5,
+    playerbackRate: 1.0,
+    played: 0,
+    seeking: false,
+  });
+  const { playing, muted, volume, playerbackRate, played, seeking } =
+    playerstate;
+  const playerRef = useRef(null);
+  const playerDivRef = useRef(null);
 
+  //This function handles play and pause onchange button
+  const handlePlayAndPause = () => {
+    setPlayerState({ ...playerstate, playing: !playerstate.playing });
+  };
+
+  const handleMuting = () => {
+    setPlayerState({ ...playerstate, muted: !playerstate.muted });
+  };
+
+  const handleRewind = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() - 10);
+  };
+
+  const handleFastForward = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() + 30);
+  };
+
+  const handleVolumeChange = (e, newValue) => {
+    setPlayerState({
+      ...playerstate,
+      volume: parseFloat(newValue / 100),
+      muted: newValue === 0 ? true : false,
+    });
+  };
+
+  const handleVolumeSeek = (e, newValue) => {
+    setPlayerState({
+      ...playerstate,
+      volume: parseFloat(newValue / 100),
+      muted: newValue === 0 ? true : false,
+    });
+  };
+
+  const handlePlayerRate = (rate) => {
+    setPlayerState({ ...playerstate, playerbackRate: rate });
+  };
+
+  const handleFullScreenMode = () => {
+    screenfull.toggle(playerDivRef.current);
+  };
+
+  const handlePlayerProgress = (state) => {
+    console.log("onProgress", state);
+    if (!playerstate.seeking) {
+      setPlayerState({ ...playerstate, ...state });
+    }
+    console.log("afterProgress", state);
+  };
+
+  const handlePlayerSeek = (e, newValue) => {
+    setPlayerState({ ...playerstate, played: parseFloat(newValue / 100) });
+    playerRef.current.seekTo(parseFloat(newValue / 100));
+    // console.log(played)
+  };
+
+  const handlePlayerMouseSeekDown = (e) => {
+    setPlayerState({ ...playerstate, seeking: true });
+  };
+
+  const handlePlayerMouseSeekUp = (e, newValue) => {
+    setPlayerState({ ...playerstate, seeking: false });
+    playerRef.current.seekTo(newValue / 100);
+  };
+
+  const currentPlayerTime = playerRef.current
+    ? playerRef.current.getCurrentTime()
+    : "00:00";
+  const movieDuration = playerRef.current
+    ? playerRef.current.getDuration()
+    : "00:00";
+  const playedTime = format(currentPlayerTime);
+  const fullMovieTime = format(movieDuration);
   
   const downloadfile = (filename) => {
     var thisurl = 'https://booeg3cke4.execute-api.us-east-1.amazonaws.com/test/file';
@@ -35,6 +139,24 @@ const ManageFiles = () => {
       });
     
   }
+  const [videourl, setVideo] = useState([]);
+
+  const playfile = (filename) => {
+    var thisurl = 'https://booeg3cke4.execute-api.us-east-1.amazonaws.com/test/file';
+    thisurl = thisurl + '?filename='+ filename;
+    axios({url: thisurl, method: 'GET'})
+      .then(response => {
+        // setVideo(response.data);
+        console.log(response);
+        const aTag = document.createElement("a");
+        aTag.href = response.data;
+        document.body.appendChild(aTag);
+        setVideo(aTag);
+        // aTag.click();
+        // aTag.remove();
+
+      });
+  }
 
   const deletefile = (filename) => {
     const choice = window.confirm("Are you sure to delete this file?")
@@ -45,7 +167,7 @@ const ManageFiles = () => {
       axios({url: thisurl, method: 'DELETE'})
         .then(response => {
           console.log(response.data);
-          
+          window.location.reload(false);
         });
     }
   }
@@ -67,9 +189,10 @@ const ManageFiles = () => {
       headerName: "Date",
       flex: 1,
     },
+    
     {
       field: "action",
-      headerName: "Action",
+      headerName: "Play",
       flex: 1,
       filterable:false,
       sortable:false,
@@ -80,10 +203,10 @@ const ManageFiles = () => {
             m="0 auto"
             p="5px"
             display="flex"
-            onClick={() => downloadfile(Key)}
+            onClick={() => playfile(Key)}
           >
             <Typography color={colors.greenAccent[100]} sx={{ ml: "5px" }}>
-              {'Download'}
+              {'Play'}
             </Typography>
           </Button>
         );
@@ -114,7 +237,6 @@ const ManageFiles = () => {
     },
   ];
 
-
   // axios.get('https://booeg3cke4.execute-api.us-east-1.amazonaws.com/test/files')
   //   .then(response => {
   //     console.log(response.data);
@@ -134,12 +256,17 @@ const ManageFiles = () => {
   useEffect(() => {
     fetchInfo();
   }, []);
+  const [value, onChange] = useState(new Date());
   
     return (
-    <Box m="20px">
+    
+    <Box m="40px">
+      
+        
+      
       <Header title="File System" subtitle="Managing the Files" />
       <Box
-        m="40px 0 0 0"
+        m="20px 0 0 0"
         height="75vh"
         sx={{
           "& .MuiDataGrid-root": {
@@ -172,8 +299,24 @@ const ManageFiles = () => {
         columns={columns} 
 
         />
+        <ReactPlayer
+            width={"50%"}
+            height="50%"
+            ref={playerRef}
+            url={videourl}
+            playing={playing}
+            volume={volume}
+            playbackRate={playerbackRate}
+            onProgress={handlePlayerProgress}
+            controls
+            loop
+            muted={muted}
+            onError={() => console.log("onError callback")}
+          />  
       </Box>
+      
     </Box>
+    
   );
     
   
